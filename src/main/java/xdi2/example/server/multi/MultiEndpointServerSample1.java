811ff62d4b621c4334afa6abb881951e91afb13d
peacekeeper <markus@projectdanube.org>
2015-02-04 18:55:01 +0100

    fixing API call to get messaging target registry

--- src/main/java/xdi2/example/server/multi/MultiEndpointServerSample1.java
+++ src/main/java/xdi2/example/server/multi/MultiEndpointServerSample1.java
@@ -20,7 +20,7 @@ public class MultiEndpointServerSample1 {
 
 		// mount messaging target
 
-		endpointServer.getEndpointServlet().getHttpMessagingTargetRegistry().mountMessagingTarget("/", messagingTarget);
+		endpointServer.getEndpointServlet().getHttpTransport().getHttpMessagingTargetRegistry().mountMessagingTarget("/", messagingTarget);
 	}
 
 	public static void main(String[] args) throws Exception {
